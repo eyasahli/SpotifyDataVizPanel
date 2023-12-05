19e881df98b5f81b9d23f2a73ec5db39c8051549
Nourbenali <61160865+nourbenali@users.noreply.github.com>
2023-12-05 10:38:11 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import panel as pn
+from bokeh.embed import server_document
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+from sliders.dash1 import create_dashboard_1
+from sliders.dash2 import create_dashboard_2
+import pandas as pd
+
+app = FastAPI()
+
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def bkapp_page(request: Request):
+    script = server_document('http://127.0.0.1:5000/app')
+    return templates.TemplateResponse("index.html", {"request": request, "script": script})
+
+
+@app.get("/explore")
+async def bkapp_page2(request: Request):
+    script = server_document('http://127.0.0.1:5000/dash1')
+    return templates.TemplateResponse("dash1.html", {"request": request, "script": script})
+
+@app.get("/predict")
+async def bkapp_page3(request: Request):
+    script = server_document('http://127.0.0.1:5000/dash2')
+    return templates.TemplateResponse("dash2.html", {"request": request, "script": script})
+
+
+pn.serve({'/dash1': create_dashboard_1,'/dash2':create_dashboard_2},
+        port=5000, allow_websocket_origin=["127.0.0.1:8000"],
+         address="127.0.0.1", show=False)
+
+
+# uvicorn main:app --reload
